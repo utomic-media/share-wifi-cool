18a8b45ae1ce6514f918f19d026bb386ac67e1ff
Dominic Marcelino <domi.m.98@googlemail.com>
2021-07-27 12:02:04 +0200

    add tailwind css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import router from '@/router/index';
 import i18n from '@/plugins/i18n/index';
 import App from './App.vue';
+import '@/styles/main.scss';
 
 createApp(App)
   .use(router)
